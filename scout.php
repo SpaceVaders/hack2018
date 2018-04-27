92d25c478c6bb5df41d46dedaa4bff3cc680e587
Borislav <33787838+bobooooooo@users.noreply.github.com>
2018-04-27 20:29:29 +0300

    test

---
+++ scout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Space Vaders Scout</title>
+	<link href="style/style_scout.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	
+</body>
+</html>
